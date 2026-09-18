6e4515b2d6ced6c78af973b455f53c394f3d827d
Simonirabella <simonirabellahobby@proton.me>
2026-09-18 10:12:29 +0200

    basic framework

--- src/mqtt/mqtt_pub.py
+++ src/mqtt/mqtt_pub.py
@@ -1,4 +1,6 @@
 import paho.mqtt.publish as publish
+import json
+from datetime import datetime, timezone
 
 def publish_message(topic: str, message: str, hostname: str = "localhost"):
     """
@@ -10,16 +12,55 @@ def publish_message(topic: str, message: str, hostname: str = "localhost"):
     """
     publish.single(topic, message, hostname=hostname)
 
-    #UserCode, StationID, Optional1, Optional2
+def create_start_message (user_code):
+    return {
 
+        "station_id": 0, #Eure Stationsnummer als Integer 1-5
+        "user_code": user_code, 
+
+        "data":
+        {
+            "startTime": datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ"), #Der aktuelle Timestamp
+        }
+    }
+
+def create_end_message (user_code, feedback):
+    return {
+
+        "station_id": 0, #Eure Stationsnummer als Integer 1-5
+        "user_code": user_code, 
+
+        "data":
+        {
+            "endTime": datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ"), #Der aktuelle Timestamp
+            "feedback": feedback
+        }
+    }
 
 {
-  "station_id": "sensor-01",
-  "user_code": "ADDXE",
-  "timestamp": "<timestamp>",
-  "difficulty": "easy, medium, hard",
-  "feedback": "happy, neutral, unhappy",
+  "station_id": "int", #1-5
+  "user_code": "int", # randomly generated 5 number code
 
+  "data":
+  {
+    "startTime": "null", #only on start message
+    "endTime": "null", #only on end message
+    "difficulty": "easy, medium, hard" ,    #only on start message, default = medium
+    "feedback": "happy, neutral, unhappy"   #only on end message
+  }
 }
-g = ("x",3,2)
-print(g)
+
+# Bei Station Start:
+topic = "devices/placeholder"
+user_code = 11111
+
+start_message = create_start_message(user_code)
+publish_message(topic, json.dumps(start_message))
+
+# Bei Station End
+feedback = "happy"
+end_message = create_end_message(user_code, feedback)
+
+# Debug
+#print(json.dumps(start_message))
+#print(json.dumps(end_message))
