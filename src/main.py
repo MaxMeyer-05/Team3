b60d7e49d0d4ae4b3203189b8c2f776f4ac9e19e
MaxMeyer-05 <maxmeyer2411@gmail.com>
2026-09-22 09:43:48 +0200

    Refactor MQTT client loop to handle KeyboardInterrupt and ensure proper disconnection

--- src/main.py
+++ src/main.py
@@ -6,5 +6,11 @@ BROKER_HOST = "localhost"
 client = mqtt.Client()
 client.on_connect = mqtt_sub.on_connect
 client.on_message = mqtt_sub.on_message
+
 client.connect(BROKER_HOST)
-client.loop_forever()
+try:
+    client.loop_forever()
+except KeyboardInterrupt:
+    pass
+finally:
+    client.disconnect()
